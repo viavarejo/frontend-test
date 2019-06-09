2b4e2c2e11e39a50eb8c3e096e61510ac3424506
Luan Sousa <luan26@uninove.edu.br>
2019-06-09 12:37:53 -0300

    Service implementado

--- src/app/components/services/transacao.service.ts
+++ src/app/components/services/transacao.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Transacao } from '../models/extrato.model';
 
 @Injectable()
 export class TransacaoService {
 
+    public saveTransaction(transaction: Transacao): void {
+        const transactions = JSON.parse(localStorage.getItem('transacoes'));
+        transactions.push(transaction);
+        localStorage.setItem('transacoes', JSON.stringify(transactions));
+    }
+
+    public getTransactions(): Array<any> {
+       return JSON.parse(localStorage.getItem('transacoes'));
+    }
 }
